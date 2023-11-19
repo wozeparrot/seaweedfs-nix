395e2a042c4b81ca48ebd4d31c81259d4e631806
Woze Parrot <wozeparrot@gmail.com>
2023-11-18 22:13:17 -0500

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "auto updating seaweedfs overlay";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    seaweedfs = {
+      url = "github:seaweedfs/seaweedfs";
+      flake = false;
+    };
+  };
+
+  outputs = inputs @ {
+    nixpkgs,
+    flake-utils,
+    ...
+  }: let
+    overlay = final: prev: {
+      seaweedfs = prev.buildGoModule rec {
+        pname = "seaweedfs";
+        version = inputs.seaweedfs.shortRev;
+        src = inputs.seaweedfs;
+
+        vendorHash = "sha256-h4YBDV9ygSo0aFLYSXp0TiwL2RgZi0OijhLmVlRANhA=";
+
+        subPackages = ["weed"];
+
+        ldflags = [
+          "-w"
+          "-s"
+          "-X github.com/seaweedfs/seaweedfs/weed/util.COMMIT=${version}"
+        ];
+
+        tags = [
+          "elastic"
+          "gocdk"
+          "sqlite"
+          "ydb"
+          "tikv"
+        ];
+
+        preBuild = ''
+          export GODEBUG=http2client=0
+        '';
+
+        preCheck = ''
+          # Test all targets.
+          unset subPackages
+
+          # Remove unmaintained tests ahd those that require additional services.
+          rm -rf unmaintained test/s3
+        '';
+      };
+    };
+  in
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [overlay];
+        };
+      in {
+        packages.default = pkgs.seaweedfs;
+      }
+    )
+    // {
+      overlays.default = overlay;
+    };
+}
---
+++ update.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p nurl
+set -euo pipefail
+IFS=$'\n\t'
+
+function get_vendor_hash() {
+    local url="$1"
+    local src="$(nurl "$url")"
+    if [[ -z "$src" ]]; then
+        echo "Failed to fetch $url" >&2
+        exit 1
+    fi
+    local stderr="$(nix build --impure --no-link --expr "with (import <nixpkgs> {}); buildGoModule {pname=\"\";version=\"\";src=$src;vendorHash=\"\";}" 2>&1 > /dev/null)"
+    # vendorHash line contains "got: "
+    local vendor_hash="$(echo "$stderr" | grep -oP '(?<=got: ).*')"
+    vendor_hash="$(echo "$vendor_hash" | tr -d '[:space:]')"
+    echo "$vendor_hash"
+}
+
+function replace_vendor_hash() {
+    local vendor_hash="$1"
+    local file="$2"
+    local tmp="$(mktemp)"
+    sed "s/vendorHash = \".*\"/vendorHash = \"$vendor_hash\"/" "$file" > "$tmp"
+    mv "$tmp" "$file"
+}
+
+function main() {
+    local url="https://github.com/seaweedfs/seaweedfs"
+    local file="./flake.nix"
+
+    local vendor_hash="$(get_vendor_hash "$url")"
+    replace_vendor_hash "$vendor_hash" "$file"
+}
+
+main
