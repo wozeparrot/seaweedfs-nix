af33382e349fbb5c866244d443c3d27c9ebcb949
seaweedfs-nix <wozeparrot@users.noreply.github.com>
2024-01-23 00:30:31 +0000

    feat: update vendorHash

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
         version = inputs.seaweedfs.shortRev;
         src = inputs.seaweedfs;
 
-        vendorHash = "sha256-WAGuaL8kDtMUDkHetWagCGZS91Y3Tg2DV2StKgRpuIg=";
+        vendorHash = "sha256-8j1YzhmXlFzz7ta1JbhebBQ1FRYZ2wIhQlmUlxyJLlk=";
 
         subPackages = ["weed"];
 
